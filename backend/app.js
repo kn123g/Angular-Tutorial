c41faef0213ac991081a8a693883e1d3eb7341c7
KARTHIKEYAN GANESH <karthiknganesh96@gmail.com>
2020-09-10 12:32:39 +0530

    Deployment full package

--- backend/app.js
+++ backend/app.js
@@ -26,10 +26,13 @@ app.use((req,res,next)=>{
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("images")));
-
+app.use("/images", express.static(path.join(__dirname,"images")));
+app.use("/", express.static(path.join(__dirname , "angular")));
 //app.use(cookieParser());
 //app.use(upload.array());
 app.use("/api/posts",postRouter);
 app.use("/api/user",userRouter);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname ,"angular","index.html"));
+});
 module.exports = app;
